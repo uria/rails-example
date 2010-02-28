a67ecc3b5cb9655bd89fa6c02fa6f0a661fffa60
Benigno Uria <benigno.uria@gmail.com>
2010-02-28 19:18:31 +0000

    Avoid photo creation if the user has already 10 of them.

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -3,7 +3,15 @@ class Photo < ActiveRecord::Base
   
   has_attached_file :photo
 
+  validates_presence_of :user
+
   validates_attachment_presence :photo
   validates_attachment_size :photo, :less_than => 1.megabytes
   validates_attachment_content_type :photo, :content_type => ['image/jpeg', 'image/jpg', 'image/png']
+  
+  private
+  
+  def validate
+    errors.add(:photo, "You can't have more than 10 photos.") if self.user.photos.size >= 10
+  end
 end
--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,10 +1,21 @@
 require 'spec_helper'
+include ActionController::TestProcess
 
 describe Photo do
   before(:each) do
-    @valid_attributes = {
-      :user_id => 1
-    }
+    @user_with_10_photos = mock_model(User)
+    @user_with_10_photos.stub(:photos).and_return((1..10).collect { |i| mock_model(Photo)})
   end
+  
+  describe "Photo creation for a user with 10 photos already" do
+    it "should have an error on photo after creation" do
+      p = Photo.new
+      p.photo = fixture_file_upload("image.jpg", "image/jpg")
+      p.user = @user_with_10_photos
+      p.save
+      p.should have(1).errors_on(:photo)
+    end
+  end
+  
 
 end
